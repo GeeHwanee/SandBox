29ded0a087e7ed21dd6a683f5b056ece6b0a1eb3
JiHwan Park <jihwanpark15@gmail.com>
2023-07-17 16:46:43 +0900

    update ICNT

---
+++ ICNT/autoCheck.js
@@ -0,0 +1,23 @@
+let title = "테니스";
+let purpose = "테니스";
+
+const action = new URLSearchParams(new URL(location.href).search);
+const isWrite = action.get('action');
+$('#main_save').click(function (){
+    title = $('#main_title').val();
+    purpose = $('#main_purpose').val();
+    alert("저장하였습니다.");
+    window.close();
+});
+$('#reservation').click(function (){
+    const $name = $('#mem_nm').val();
+    $('#team_nm').val($name);
+    $('#users').val('4');
+    $('#title').val(title);
+    $('#purpose').val(purpose);
+    $('#agree_use1').prop('checked',true);
+    $('#writeBtn').click();
+})
+$(document).on('click', '.action_check', function() {
+   $('.nowrap').append($('<button id="reservation">예약</button>'))
+});
